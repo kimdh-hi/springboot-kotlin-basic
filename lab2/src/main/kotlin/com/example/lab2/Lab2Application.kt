b054f9cda224daabbdd6450e23c4603c6babc1fc
kimdh-hi <zbeld123@gmail.com>
2021-12-13 12:55:51 +0900

    lab2 - kotlin + JPA(hibernate) Init

---
+++ lab2/src/main/kotlin/com/example/lab2/Lab2Application.kt
@@ -0,0 +1,11 @@
+package com.example.lab2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab2Application
+
+fun main(args: Array<String>) {
+    runApplication<Lab2Application>(*args)
+}
